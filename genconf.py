7ef4ce103e1172243599e79d2d995e83bda3df0d
Reed <reed@plusreed.com>
2017-02-15 17:44:38 -0500

    Adding onto genconf.py, horrendously broken right now
    Signed-off-by: Reed <reed@plusreed.com>

--- genconf.py
+++ genconf.py
@@ -1,6 +1,8 @@
 # genconf.py
 # Generate a fox_config.json
 
+import os
+import sys
 import json
 import click
 
@@ -12,3 +14,16 @@ def welcome():
     print("genconf.py")
     print("ver 0.1, beta")
     print("Please report bugs to https://github.com/plusreed/foxpy\n")
+    sanity()
+
+def sanity(stage):
+    # Sanity checks
+    # We need to check the working directory for 'fox_config.json'
+    stage = 1
+    if os.path.isfile("..\fox_config.json"):
+        print("fox_config.json already exists.")
+        stage = 0
+        sys.exit()
+    else:
+        # Passed stage 1 sanity check
+        # Currently this is the only sanity stage, there should be more probably
