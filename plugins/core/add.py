9b15117e814b7e6a137508057dee043baa1cfe41
Reed <reed@plusreed.com>
2017-02-15 17:45:01 -0500

    Add plugins/core folder, begin work on add.py
    Signed-off-by: Reed <reed@plusreed.com>

---
+++ plugins/core/add.py
@@ -0,0 +1,9 @@
+# add.py - CORE
+# Fox plugin for adding numbers together
+
+
+def add(num1, num2):
+    number1 = float(num1)
+    number2 = float(num2)
+    val = number1 + number2
+    # print(val)
