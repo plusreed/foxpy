55d202a8f0eb0eef39ae5519180096cee298d4fa
Reed <reed@plusreed.com>
2017-02-16 17:29:17 -0500

    genconf SHOULD work. Should.
    Signed-off-by: Reed <reed@plusreed.com>

--- genconf.py
+++ genconf.py
@@ -1,29 +1,34 @@
 # genconf.py
 # Generate a fox_config.json
 
+
 import os
 import sys
 import json
 import click
 
-@click.command()
-@click.option('--new-conf', prompt='New configuration name', help='Generate a new configuration file')
 
-def welcome():
-    """Function that welcomes the user"""
-    print("genconf.py")
-    print("ver 0.1, beta")
-    print("Please report bugs to https://github.com/plusreed/foxpy\n")
-    sanity()
+@click.command()
+@click.option("--new-conf", prompt="New configuration name", help="Generate a new configuration file")
 
 def sanity(stage):
     # Sanity checks
     # We need to check the working directory for 'fox_config.json'
-    stage = 1
     if os.path.isfile("..\fox_config.json"):
         print("fox_config.json already exists.")
-        stage = 0
-        sys.exit()
+        del stage  # As a sanity check failed, stage will be removed.
+        sys.exit()  # Exit genconf.
     else:
         # Passed stage 1 sanity check
         # Currently this is the only sanity stage, there should be more probably
+        print("Sanity checks passed.")
+
+
+def welcome():
+    """Welcome function for CLI"""
+    print("genconf.py")
+    print("ver 0.1, beta")
+    print("Please report bugs to https://github.com/plusreed/foxpy")
+    sanity()
+
+welcome()
