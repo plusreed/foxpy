08c2e9144e605063ac3c6313efe639eb7139ba75
Reed <reed@plusreed.com>
2017-02-15 20:44:35 -0500

    Add import statements for plugin system
    Signed-off-by: Reed <reed@plusreed.com>

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 # Fox, rewritten in Python for literally no reason at all.
 
+
 import discord
 import asyncio
+import plugins
+import plugins.core
+
 
 print("Just a moment, Fox is initializing...")
 fox = discord.Client()
+
 
 @fox.event
 async def on_ready():
