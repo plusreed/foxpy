89f82bb33542be1f77b2ab7bc129b3806b3dc369
Reed <reed@plusreed.com>
2017-02-16 16:35:28 -0500

    Removing broken stuff
    Signed-off-by: Reed <reed@plusreed.com>

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -10,11 +10,11 @@ class fplg:
     @staticmethod
     def plugin_error(level, message):
         if level == 0:  # Info
-            print("[fplg.INFO]: " + fplg.plugin_error.message)
+            print("[fplg.INFO]: " + message)
         elif level == 1:  # Warning
-            print("[fplg.WARN]: " + fplg.plugin_error.message)
+            print("[fplg.WARN]: " + message)
         elif level == 2:  # Error
-            print("[fplg.ERR]: " + fplg.plugin_error.message)
+            print("[fplg.ERR]: " + message)
         elif level > 2:  # Level is bigger than 2
             print("[fplg.ERR]: fplg.plugin_error only uses error severity levels 0-2.")
 
