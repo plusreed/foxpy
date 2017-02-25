22bc962aebe0ba65ffbed85f0654ea908280abf7
Reed <reed@plusreed.com>
2017-02-24 19:35:51 -0500

    well, here it is, eval
    Signed-off-by: Reed <reed@plusreed.com>

--- plugins/admin/eval.py
+++ plugins/admin/eval.py
@@ -9,45 +9,6 @@ import datetime
 from collections import Counter
 
 
-class FoxEval:
-    async def evalpy(statement, message):
-        try:
-            evaled = eval(statement)
-            em = fox.Embed(title='eval()', description="Here's what I got for that:\n" + evaled, colour=0x00AF19)
-            em.set_author(name='Fox', icon_url=fox.user.default_user_url)
-            await fox.send_message(message.channel, embed=em)
-            return
-        except:
-            exc = Exception
-            em = fox.Embed(title='eval()', description="I tried doing that but encountered an error.\n" + exc,
-                           colour=0x910000)
-            em.set_author(name='Fox', icon_url=fox.user.default_user_url)
-            await fox.send_message(message.channel, embed=em)
-            return
-
-"""
-@fox.event
-async def on_message(a_message):
-    if a_message.content.startswith(plg_cmd):
-        statement = a_message.content
-        try:
-            final_eval = eval(statement)
-            em = fox.Embed(title="eval()", description="Here's what I gof for that:\n" + final_eval, colour=0x00AF19)
-            em.set_author(name="Fox", icon_url=fox.user.default_user_url)
-            await fox.send_message(a_message.channel, embed=em)
-            return
-        except:
-            exc = Exception
-            em = fox.Embed(title='eval()', description="I tried doing that but encountered an error.\n" + exc,
-                           colour=0x910000)
-            em.set_author(name='Fox', icon_url=fox.user.default_user_url)
-            await fox.send_message(a_message.channel, embed=em)
-            return
-
-plg_cmd = "$%eval"
-"""
-
-
 class Eval:
     """Evaluator command for admins."""
     def __init__(self, bot):
