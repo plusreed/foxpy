c086110b631b9af61eb856e0d5e6c03c12be9470
Reed <reed@plusreed.com>
2017-02-25 04:11:40 -0500

    PEP fixes + fixed a whoopsie
    Signed-off-by: Reed <reed@plusreed.com>

--- main3.py
+++ main3.py
@@ -33,8 +33,9 @@ hattr = dict(hidden=True)
 prefix = ['$', '^']
 fox = commands.Bot(command_prefix=prefix, description=description, pm_help=None, help_attrs=hattr)
 
+
 @fox.event
-async def on_command_error(error, ctx)
+async def on_command_error(error, ctx):
     if isinstance(error, commands.NoPrivateMessage):
         await fox.send_message(ctx.message.author, "Sorry, you can't use this command in private messages.")
     elif isinstance(error, commands.DisabledCommand):
@@ -43,6 +44,7 @@ async def on_command_error(error, ctx)
         print('In {0.command.qualified_name}:'.format(ctx), file=sys.stderr)
         traceback.print_tb(error.original.__traceback__)
         print('{0.__class__.__name__}: {0}'.format(error.original), file=sys.stderr)
+
 
 @fox.event
 async def on_ready():
@@ -53,9 +55,11 @@ async def on_ready():
     if not hasattr(fox, 'uptime'):
         fox.uptime = datetime.datetime.utcnow()
 
+
 @fox.event
 async def on_resumed():
     print("Fox has resumed.")
+
 
 @fox.event
 async def on_command(command, ctx):
@@ -68,6 +72,7 @@ async def on_command(command, ctx):
         destination = '#{0.channel.name} ({0.server.name})'.format(message)
 
     log.info('{0.timestamp}: {0.author.name} in {1}: {0.content}'.format(message, destination))
+
 
 @fox.event
 async def on_message(message):
